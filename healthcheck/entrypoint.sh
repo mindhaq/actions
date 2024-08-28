cb47fb64f65729ff5bef04684e38fc7dd92fd83b
Rüdiger Schulz <code@mindhaq.com>
2024-08-28 22:43:32 +0200

    #2: ignore empty username or password

--- healthcheck/entrypoint.sh
+++ healthcheck/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -l
 
 if [ $# -lt 2 ] ; then
-    echo "Usage: $0 url retries [user] [password]"
+    echo "Usage: $0 url retries [username password]"
     exit 1
 fi
 
 curloptions="--output /dev/null -silent --show-error --retry $2 --fail"
 
-if [ $# -eq 4 ] ; then
+if [ $# -eq 4 ] && [ -n "$3" ] && [ -n "$4" ] ; then
     curloptions="$curloptions --user $3:$4"
 fi
 
